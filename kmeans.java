e629bbef0a806fb98f186d37189045f66fc819ce
ajinkyablaze <jumbadajinkya@gmail.com>
2018-03-04 09:22:49 +0530

    kmeans and single_pass

---
+++ kmeans.java
@@ -0,0 +1,203 @@
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class kmeans {
+
+    final int max_v = 100, var = 0;
+    final int clusters_total;
+    int epoch = 0;
+    final int MAX_EPOCHS = 1000;
+    ArrayList<cluster> clusters = new ArrayList<>();
+    ArrayList<data> datas = new ArrayList<>();
+    HashMap<Integer, Integer> hm = new HashMap<>();
+    HashMap<Integer, ArrayList<Integer>> ans = new HashMap<>();
+
+    class data {
+
+        final int data_hash;
+        cluster c;
+        final float x, y;
+
+        data(float x, float y, int data_hash) {
+            this.x = x;
+            this.y = y;
+            this.data_hash = data_hash;
+        }
+
+    }
+
+    class cluster {
+
+        final int cluster_no;
+        float x, y;
+        float sum_x = 0, sum_y;
+        int num = 0;
+
+        cluster(float x, float y, int cluster_no) {
+            this.x = x;
+            this.y = y;
+            this.cluster_no = cluster_no;
+        }
+
+        float distance(float x, float y) {
+            return (float) (Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2));
+        }
+
+        void sum_add(float sumx, float sumy) {
+            this.sum_x += sumx;
+            this.sum_y += sumy;
+            num++;
+        }
+
+        void new_centroid() {
+//System.out.println(
+            this.x = this.sum_x / num;
+            this.y = this.sum_y / num;
+            sum_x = 0;
+            sum_y = 0;
+            num = 0;
+        }
+
+    }
+
+    void find_cluster(data d) {
+        float min = Float.MAX_VALUE;
+        cluster c_final = null;
+        for (cluster c : clusters) {
+            float dis = c.distance(d.x, d.y);
+            if (dis < min) {
+                min = dis;
+                c_final = c;
+            }//end if
+        }//end for
+        d.c = c_final;
+
+    }
+
+    void assign() {
+
+        for (data d : datas) {
+            find_cluster(d);
+        }
+
+    }//end assign
+
+    void calc_new_centroid() {
+
+        for (data d : datas) {
+            d.c.sum_add(d.x, d.y);
+        }
+
+        for (cluster c : clusters) {
+            c.new_centroid();
+        }
+
+    }//end func
+
+    void print() {
+
+        for (cluster c : clusters) {
+            System.out.println("Cluster No. " + c.cluster_no + " || Centroid -> " + c.x + " " + c.y);
+        }
+
+        for (data d : datas) {
+            System.out.println(d.x + " " + d.y + " || cluster No. " + d.c.cluster_no);
+        }
+
+        System.out.println("----------------------------------------------");
+    }
+
+    boolean stop_check() {
+        boolean b = true;
+        for (data d : datas) {
+            int c_no = hm.get(d.data_hash);
+            if (c_no != d.c.cluster_no) {
+                b = false;
+                break;
+            }//end if
+        }//end for
+
+        return b;
+
+    }
+
+    void dump_data() {
+        hm.clear();
+        for (data d : datas) {
+            hm.put(d.data_hash, d.c.cluster_no);
+        }
+
+    }
+
+    void populate_ans() {
+        for (data d : datas) {
+            if (ans.get(d.c.cluster_no) == null) {
+                ans.put(d.c.cluster_no, new ArrayList<>());
+            }
+
+            ans.get(d.c.cluster_no).add(d.data_hash);
+        }
+
+        for (cluster c : clusters) {
+            System.out.println("Cluster no " + c.cluster_no);
+            ArrayList<Integer> ar = ans.get(c.cluster_no);
+            for (int i : ar) {
+                System.out.print("#" + i + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    
+    
+    void random_input(Scanner s){
+        Random r = new Random();
+        
+        for (int i = 0; i < clusters_total; i++) {
+            clusters.add(new cluster(r.nextInt(max_v) - var, r.nextInt(max_v) - var, i));
+        }
+
+        System.out.println("Enter number of input data");
+        int inputs = s.nextInt();
+        for (int i = 0; i < inputs; i++) {
+            datas.add(new data(r.nextInt(max_v) - var, r.nextInt(max_v) - var, i));
+        }
+    }
+
+    void run_epochs(){
+        boolean first = true;
+        for (int i = 0; i < MAX_EPOCHS; i++) {
+            epoch++;
+            assign();
+            calc_new_centroid();
+            //print();
+            if (!first) {
+                if (stop_check()) {
+                    break;
+                }
+            }//end if
+            first = false;
+            dump_data();
+        }//end for
+        System.out.println("Number of epochs is/are " + epoch);
+    }
+    
+    kmeans() {
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter number of clusters");
+        clusters_total = s.nextInt();       
+        random_input(s);
+        run_epochs();
+        populate_ans();
+
+    }//end fun
+
+    public static void main(String args[]) {
+        new kmeans();
+    }
+
+}
---
+++ single_pass.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+#include <vector>
+#include<algorithm>
+#include<math.h>
+
+using namespace std;
+
+class document{
+    
+    public:
+    vector<string> terms;
+    int degree = 0;
+    
+    
+    document(){
+        string s;
+        int i,j;
+        //manual add
+        cout<<"no of terms\n";
+        cin>>i;
+        for(j=0;j<i;j++){
+            cin>>s;
+            terms.push_back(s);
+        }//end for
+        do_sort();
+     
+    }
+    
+    
+    float size(){
+       return sqrt(terms.size()); 
+    }
+    
+    void print(){
+        for (auto i = terms.begin(); i != terms.end(); ++i){
+            cout << *i << ' ';
+        }
+        cout<<"\n";
+    }
+    
+    void do_sort(){
+        sort(terms.begin(), terms.end());
+    }
+    
+    
+};
+
+
+
+class cluster{
+    
+    vector<document*> docs;
+    document *cr;
+    
+    void new_cr(document *d){
+        cr = d;
+    }
+    
+    public:
+    
+    void add_doc(document *d){
+        docs.push_back(d);
+        new_cr(d);
+    }
+    
+    float dice_coeff(document *d){
+        //intersection between cr and d
+        int intersection = intersect(cr->terms,d->terms);
+        //cout<<intersection<<"\n";
+        //cr->print();
+        //d->print();
+        float dice = ((float)intersection)/(cr->size() * d->size());
+        cout<<intersection<<" "<<cr->size()<<" "<<d->size()<<" dice \n";
+        return dice;
+    }
+    
+    int intersect(vector<string> &v1, vector<string> &v2){
+
+        //vector<string> v11 = new vector(v1);
+        //vector<string> v22 = new vector(v2);
+        vector<string> v3;
+        
+        //sort(v1.begin(), v1.end());
+        //sort(v2.begin(), v2.end());
+        
+        set_intersection(v1.begin(),v1.end(),v2.begin(),v2.end(),back_inserter(v3));
+        
+        return v3.size();
+        
+    }
+    
+    
+};
+
+
+vector<cluster> cl;
+
+void determine_cluster(document *d){
+    int new_cluster=1; //flag
+    float dc;
+    cluster *best_cluster;
+    int max = 0; //for best cluster
+    float threshold = 0.6;
+    if(cl.size() == 0){
+        cluster c;
+        c.add_doc(d);
+        cl.push_back(c);
+    }else{
+        for (vector<cluster>::iterator it = cl.begin() ; it != cl.end(); ++it){
+            dc = it->dice_coeff(d);
+            cout<<dc<<"\n";
+            if(dc > max){
+                max = dc;
+                best_cluster = &*it;
+                //it->add_doc(d);
+                //new_cluster = 0;
+                //break;
+            }
+        }
+        
+        if(max > threshold){
+            best_cluster->add_doc(d);
+            new_cluster = 0;
+        }
+        
+        if(new_cluster){
+            cluster c;
+            c.add_doc(d);
+            cl.push_back(c);
+            cout<<"new cluster made"<<"\n";
+        }
+        
+        
+    }
+    
+    
+    
+}
+
+
+int main() {
+    int total_docs,i;
+    cout<<"No. of documents\n";
+    cin>>total_docs;
+    i = total_docs;
+    while(i--){
+        determine_cluster(new document());
+    }
+    
+    cout<<"No. of clusters formed is/are "<<cl.size()<<"\n";
+    
+	return 0;
+}
